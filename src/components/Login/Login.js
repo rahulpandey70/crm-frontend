7be3f5ea0ba3e092122868befa24af01a41f42ab
rahulpandey70 <rahulpandey9103@gmail.com>
2023-04-10 10:44:18 +0530

    Created login file and update dashboard file

---
+++ src/components/Login/Login.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+
+const Login = () => {
+	return (
+		<Container>
+			<Row>
+				<Col>
+					<h1 className="text-info text-center">Login</h1>
+					<hr />
+					<Form className="mt-4">
+						<Form.Group className="mt-2">
+							<Form.Label>Email Address</Form.Label>
+							<Form.Control
+								type="email"
+								name="email"
+								placeholder="Enter Email"
+								required
+							/>
+						</Form.Group>
+						<Form.Group className="mt-2">
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type="password"
+								name="password"
+								placeholder="Enter Password"
+								required
+							/>
+						</Form.Group>
+						<Button type="submit" className="mt-2">
+							Login
+						</Button>
+					</Form>
+					<hr />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<a href="#">Forget Password?</a>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default Login;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "./Dashboard.css";
+import Login from "../../components/Login/Login";
 
 const Dashboard = () => {
 	return (
 		<div className="dashboard-page bg-info">
-			<Card>hfhgh</Card>
+			<Card className="card-box">
+				<Login />
+			</Card>
 		</div>
 	);
 };
