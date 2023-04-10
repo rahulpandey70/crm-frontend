ec55b8f37bd6144f8a6524f01b0aedee5e3b4374
rahulpandey70 <rahulpandey9103@gmail.com>
2023-04-10 13:02:07 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Default from "./Layout/Default/Default";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
 	return (
 		<div className="App">
-			<Dashboard />
+			<Default>
+				{/* <Dashboard /> */}
+				//Dashbord area
+			</Default>
 		</div>
 	);
 }
