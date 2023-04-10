01a6f88fe45641bcb03fc1f626349deb0e8d9b6b
rahulpandey70 <rahulpandey9103@gmail.com>
2023-04-10 10:15:10 +0530

    Renamed files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./pages/login/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
 	return (
 		<div className="App">
-			<Login />
+			<Dashboard />
 		</div>
 	);
 }
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+	return (
+		<div className="dashboard-page bg-info">
+			<Card>hfhgh</Card>
+		</div>
+	);
+};
+
+export default Dashboard;
