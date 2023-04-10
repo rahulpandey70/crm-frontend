cffe812e3107515a6eea5bd175e1994ded581ed5
rahulpandey70 <rahulpandey9103@gmail.com>
2023-04-10 09:56:45 +0530

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./page/login/Login";
 
 function App() {
-	return <div className="App">helllloooo</div>;
+	return (
+		<div className="App">
+			<Login />
+		</div>
+	);
 }
 
 export default App;
