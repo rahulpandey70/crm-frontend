77ceeb9ba2ab4ba590c578820c93d908a4dce900
rahulpandey70 <rahulpandey9103@gmail.com>
2023-04-10 10:12:15 +0530

    Created Login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Login from "./page/login/Login";
+import Login from "./pages/login/Login";
 
 function App() {
 	return (
---
+++ src/pages/login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import "./Login.css";
+
+const Login = () => {
+	return (
+		<div className="login-page bg-info">
+			<Card>hfhgh</Card>
+		</div>
+	);
+};
+
+export default Login;
